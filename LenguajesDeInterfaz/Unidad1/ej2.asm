0a235e233a188f9b8f624e00c799c8c0ac83e387
Jacob <jego.86@hotmail.com>
2020-02-06 12:16:48 -0700

    Ejemplo modo de direcionamiento.

---
+++ LenguajesDeInterfaz/Unidad1/ej2.asm
@@ -0,0 +1,29 @@
+;;;;Modos de direccionamiento;;;;;
+section .data
+	msj1 db "1234",10
+	len1 equ $-msj1
+	msj2 db "1234",10
+	len2 equ $-msj2
+section .bss
+	global main
+section .text
+main:
+	; msj2 = msj1
+	mov rax,[msj1]
+	mov [msj2],rax
+	;Imprimir msj1
+	mov rax,4
+	mov rbx,1
+	mov rcx,msj1
+	mov rdx,len1
+	int 80h
+	;Imprimir msj2
+	mov rax,4
+	mov rbx,1
+	mov rcx,msj2
+	mov rdx,len2
+	int 80h
+	; Salir del programa
+	mov rax,1
+	mov rbx,0
+	int 80h
