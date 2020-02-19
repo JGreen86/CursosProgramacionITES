9eccf640878776e4ef58c8ab66954462194c24df
Jacob <jego.86@hotmail.com>
2020-02-18 21:43:49 -0700

    Algoritmo actualizado.

--- MetodosNumericos/TV/Unidad2/biseccion.py
+++ MetodosNumericos/TV/Unidad2/biseccion.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plt
 
 def fx(x):
     return x**10 - 1
-
+def fx2(x):
+    return (-0.5*x**2)+(2.5*x)+4.5
 def generarGrafica(xi, xs, d):
     Lx = []
     Ly = []
     Ly2 = []
     x = xi
     while(x <= xs):
-        y = fx(x)
+        y = fx2(x)
         Lx.append(x)
         Ly.append(y)
         Ly2.append(0)
@@ -17,21 +18,23 @@ def generarGrafica(xi, xs, d):
     plt.plot(Lx,Ly,Lx,Ly2)
     plt.show()
 
-generarGrafica(0,1.3,0.01)
+generarGrafica(5,10,0.01)
 
 def biseccion(xi, xs, es):
     xr = 0
+    fxi = fx2(xi)
     while True:
         xrAnt = xr
         xr = (xi + xs) / 2.0
-        fxi = fx(xi)
-        fxs = fx(xs)
-        fxr = fx(xr)
+        #fxi = fx(xi)
+        #fxs = fx(xs)
+        fxr = fx2(xr)
         c = fxi * fxr
         if(c < 0):
             xs = xr
         elif(c > 0):
             xi = xr
+            fxi = fxr
         else:
             return xr
         ea = ((xr - xrAnt) / xr) * 100.0
@@ -39,8 +42,8 @@ def biseccion(xi, xs, es):
             break
     return xr
 
-raiz = biseccion(0,1.3,0.001)
+raiz = biseccion(5,10,0.001)
 print("raiz = " + str(raiz))
-print("f("+str(raiz)+") = "+str(fx(raiz)))
+print("f("+str(raiz)+") = "+str(fx2(raiz)))
 
 
