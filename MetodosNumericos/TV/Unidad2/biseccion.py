f9e51bfe29481e724bdb35a7a2a9f7c6d1746cf8
Jacob <jego.86@hotmail.com>
2020-02-18 21:22:34 -0700

    Algoritmo actualizado.

--- MetodosNumericos/TV/Unidad2/biseccion.py
+++ MetodosNumericos/TV/Unidad2/biseccion.py
@@ -18,3 +18,29 @@ def generarGrafica(xi, xs, d):
     plt.show()
 
 generarGrafica(0,1.3,0.01)
+
+def biseccion(xi, xs, es):
+    xr = 0
+    while True:
+        xrAnt = xr
+        xr = (xi + xs) / 2.0
+        fxi = fx(xi)
+        fxs = fx(xs)
+        fxr = fx(xr)
+        c = fxi * fxr
+        if(c < 0):
+            xs = xr
+        elif(c > 0):
+            xi = xr
+        else:
+            return xr
+        ea = ((xr - xrAnt) / xr) * 100.0
+        if(abs(ea) < es):
+            break
+    return xr
+
+raiz = biseccion(0,1.3,0.001)
+print("raiz = " + str(raiz))
+print("f("+str(raiz)+") = "+str(fx(raiz)))
+
+
